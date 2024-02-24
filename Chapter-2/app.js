4f4461ead0b8b2766a1f7b92584087c747c75bd9
ZohaibDilawar <156295575+ZohaibDilawar@users.noreply.github.com>
2024-02-24 07:55:29 +0500

    Chapter 2 done

---
+++ Chapter-2/app.js
@@ -0,0 +1,13 @@
+var jhone = "Jhone Doe"
+var years = "15 years old"
+var Development = "Certified Mobile Application Development"
+var pizza = " Pizza\n Pizz\n Piz\n Pi\n P"
+var email = "My email adderss is example@example.com"
+var book = "I am trying to learn from the  book A smarter \n way to learn JavaScript"
+
+alert(jhone)
+alert(years)
+alert(Development)
+alert(pizza)
+alert(email)
+alert(book)
