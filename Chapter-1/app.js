215dd29753ba2372d89d3e3a510a3bf73cde5f18
ZohaibDilawar <156295575+ZohaibDilawar@users.noreply.github.com>
2024-02-24 07:52:48 +0500

    Chapter 1 done

---
+++ Chapter-1/app.js
@@ -0,0 +1,10 @@
+var password ="Error! Please enter a valid password"
+var jsLand ="Welcome to JS land...\n Happy Coding!"
+var Land ="Welcome to JS land.."
+var Coding ="Happy Coding!"
+
+
+alert(password)
+alert(jsLand)
+alert(Land)
+alert(Coding)
