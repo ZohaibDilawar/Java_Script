7a89d56d60bc8bedb3555edc0f87679436dc1f23
ZohaibDilawar <156295575+ZohaibDilawar@users.noreply.github.com>
2024-03-09 08:17:27 +0500

    chapter 12-13

---
+++ chapter-12-13/app.js
@@ -0,0 +1,102 @@
+// var result= +prompt("what is your name")
+// if (result >= 150){
+// console.log("pass");
+// document.write("pass")
+// }
+// else{
+//     console.log("fail");
+//     document.write("fail")
+// }
+
+// var Html = +prompt("Html Quiz")
+// var Css = +prompt(" Css Quiz")
+// var result = ("Html + Css")
+// if (result >=150) {
+//   console.log("Pass");
+//   document.write("pass")
+// }
+// else{
+//     console.log("fail");
+//     document.write("fail")
+// }
+// var urdu = +prompt("percentage")
+// var english = +prompt("persantag")
+// var math =  +prompt("persantag")
+// var num1 = +prompt("precentage")
+// if (num1 >90){
+//   console.log("A");
+// }
+// else if (num1 > 75){
+//      console.log("B");
+// }
+// else if (num1 > 55){
+//   console.log("C");
+// }
+// else{
+//   console.log("fail");
+// }
+
+// let urdu = +prompt("percentage")
+// let math = +prompt("percentage")
+// let english = +prompt("percentage")
+// let islamiat = +prompt("percentage")
+// let plus = (urdu + math + english + islamiat)/400 *100
+// console.log("percentage", plus,"%");
+//  if (plus >= 55){
+//   console.log("pass");
+//  }
+//  else("fail")
+// var num1= +prompt("number")
+// if (num1 > 0){
+//    console.log("positive");
+// }
+// else if(num1===0){
+//   console.log("NUtral");
+// }
+// else{
+//   console.log("nagivite");
+// }
+
+
+// var password = 446688
+// var password= +prompt("enter your passwords")
+// if (password ==446688) {
+//     console.log("correct password");
+//     alert("correct password");
+// }
+// else {
+//     console.log("Incorrect password");
+//     alert("Incorrect password");
+// }
+
+// Marksheet
+var English = +prompt("English percentage")
+var Islmit = +prompt("Islmit percentage")
+var urdu = +prompt("urde percentage")
+var Computer = +prompt("Computer percentage")
+var Physics = +prompt("Physics percentage")
+var Math = +prompt("Math percentage")
+ 
+var percentage = ( "Your percentage"+"English  +Islmit+  urdu +Computer+  Physics+ Math "+"%")/600 *100
+alert("percentage")
+
+if (percentage = 450){
+    console.log("A Gride");
+    alert("A Grid")
+}
+ else if (percentage = 350){
+    console.log("B Gride");
+    alert("B Grid")
+}
+else if (percentage = 250){
+    console.log("C Gride");
+    alert("C Grid")
+}
+else if (percentage = 175){
+    console.log("D Grid");
+    alert("D Gride")
+}
+else{
+    console.log("fail");
+    alert("Fail")
+}
