93a41de693a088a9067e0304d69e4e8d7ed2e893
ZohaibDilawar <156295575+ZohaibDilawar@users.noreply.github.com>
2024-03-14 06:58:28 +0500

    chapter-14-16 done

---
+++ chapter-14-16/app.js
@@ -0,0 +1,6 @@
+var student =[]
+var stringArrary =["SSC" ,"HSC", "BCS", "BS", "BCOM", "MS", "M.Phil", "PhD "]
+var numbersArray =[1, 2, 3, 4, 5, 6, 7, 8]
+var boleanArray =[true, false]
+var mixedArray =["SSC", "HSC", "BCS", "BS", "BCOM", "MS", "M.Phil", "PhD", 1, 2, 3, 4, 5, 6, 7, 8, true, false]
+
