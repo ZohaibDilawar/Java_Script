00d15332e8915cef479414e3819fb68746a55452
ZohaibDilawar <156295575+ZohaibDilawar@users.noreply.github.com>
2024-03-15 22:25:20 +0500

    question-2 complete

--- chapter-14-16/app.js
+++ chapter-14-16/app.js
@@ -4,3 +4,15 @@ var numbersArray =[1, 2, 3, 4, 5, 6, 7, 8]
 var boleanArray =[true, false]
 var mixedArray =["SSC", "HSC", "BCS", "BS", "BCOM", "MS", "M.Phil", "PhD", 1, 2, 3, 4, 5, 6, 7, 8, true, false]
 
+
+// Question:02
+
+var studentNames =["Usama", "Zohaib","Naveed"]
+var studentScores =[320, 300, 340]
+var student1Percentage =studentScores[0]/500*100
+var student2Percentage =studentScores[1]/500*100
+var student3Percentage =studentScores[2]/500*100
+
+document.write("Score of "+studentNames[0]+' '+studentScores[0]+' '+student1Percentage+'%' +"<br>");
+document.write("Score of "+studentNames[1]+' '+studentScores[1]+' '+student2Percentage+'%'  +"<br>");
+document.write("Score of "+studentNames[2]+' '+studentScores[2]+' '+student3Percentage+'%' +"<br>");
