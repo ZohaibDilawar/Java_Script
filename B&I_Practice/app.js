fed0a921d0bfc7cc0adbe4742ff910b19471cea4
ZohaibDilawar <156295575+ZohaibDilawar@users.noreply.github.com>
2024-05-18 11:25:35 +0500

    B&I_Practice done

---
+++ B&I_Practice/app.js
@@ -0,0 +1,24 @@
+
+var pary=document.getElementById('pary')
+
+function btn_1() {
+   if (pary.style.fontWeight=="800"){
+
+        pary.style.fontWeight="400"
+   }else{
+        pary.style.fontWeight="800"
+
+   }
+
+}
+
+
+function btn_2() {
+   
+    if (pary.style.fontStyle=="italic") {
+
+        pary.style.fontStyle="normal"
+    }else{
+        pary.style.fontStyle="italic"
+    }
+}
